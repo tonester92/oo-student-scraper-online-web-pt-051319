4d912d8fd42c646ce2845ee23bf9ed36c29846fd
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-10 01:46:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,20 +9,20 @@ attr_accessor :students
     html = open(index_url)
     index = Nokogiri::HTML(open(index_url))
     index.css("div.student-card").each do |student|
-      student_information = {
+      student_information = {}
       student_information[:name] = student.css("h4.student-name").text
       student_information[:location] = student.css("p.student-location").text
       profile_path = student.css("a").attribute("href").value
-      student_information[:profile_url] = student.css('./fixtures/student-site/') + profile_path}
+      student_information[:profile_url] = student.css('./fixtures/student-site/') + profile_path
       students << student_information
     end
     students
   end
 
   def self.scrape_profile_page(profile_url)
-    student_profile = {
+    student_profile = {}
     html = open(profile_url)
-    profile = Nokogiri::HTML(html)}
+    profile = Nokogiri::HTML(html)
     profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
       if social.attribute("href").value.include?("twitter")
         student_profile[:twitter] = social.attribute("href").value
