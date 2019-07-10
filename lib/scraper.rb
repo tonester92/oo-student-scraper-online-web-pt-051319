aa4d9fbfc2c7879219c67b9beb77293089974b85
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-10 01:51:51 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,17 +26,17 @@ attr_accessor :students
     profile_doc.css("div.social-icon-container a").each do |link_xml|
       case link_xml.attribute("href").value
       when /twitter/
-        attributes[:twitter] = link_xml.attribute("href").value
+        student_attributes[:twitter] = link_xml.attribute("href").value
       when /github/
-        attributes[:github] = link_xml.attribute("href").value
+        student_attributes[:github] = link_xml.attribute("href").value
       when /linkedin/
-        attributes[:linkedin] = link_xml.attribute("href").value
+        student_attributes[:linkedin] = link_xml.attribute("href").value
       else
-          attributes[:blog] = link_xml.attribute("href").value
+          student_attributes[:blog] = link_xml.attribute("href").value
       end
     end
-    attributes[:profile_quote] = profile_doc.css("div.profile-quote").text
-    attributes[:bio] = profile_doc.css("div.bio-content div.description-holder").text.strip
-    attributes
+   student_attributes[:profile_quote] = profile_doc.css("div.profile-quote").text
+   student_attributes[:bio] = profile_doc.css("div.bio-content div.description-holder").text.strip
+   student_attributes
   end
 end
