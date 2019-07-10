32432038d476f45cf527183c631531892a2042a2
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-10 01:53:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,21 +22,21 @@ attr_accessor :students
   def self.scrape_profile_page(profile_url)
     profile_html = open(profile_url)
     profile_doc = Nokogiri::HTML(profile_html)
-    student_attributes = {}
+    attributes = {}
     profile_doc.css("div.social-icon-container a").each do |link_xml|
-      case link_xml.student_attribute("href").value
+      case link_xml.attribute("href").value
       when /twitter/
-        student_attributes[:twitter] = link_xml.attribute("href").value
+        attributes[:twitter] = link_xml.attribute("href").value
       when /github/
-        student_attributes[:github] = link_xml.attribute("href").value
+        attributes[:github] = link_xml.attribute("href").value
       when /linkedin/
-        student_attributes[:linkedin] = link_xml.attribute("href").value
+        attributes[:linkedin] = link_xml.attribute("href").value
       else
-        student_attributes[:blog] = link_xml.attribute("href").value
+          attributes[:blog] = link_xml.attribute("href").value
       end
     end
-   student_attributes[:profile_quote] = profile_doc.css("div.profile-quote").text
-   student_attributes[:bio] = profile_doc.css("div.bio-content div.description-holder").text.strip
-   student_attributes
+    attributes[:profile_quote] = profile_doc.css("div.profile-quote").text
+    attributes[:bio] = profile_doc.css("div.bio-content div.description-holder").text.strip
+    attributes
   end
 end
